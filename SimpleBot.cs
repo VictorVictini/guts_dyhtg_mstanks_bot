40f567799078c0044c75272c10341bcc221429e0
Github_Coder329423 <Github_sstealthblue@gmail.com>
2024-10-12 21:08:05 +0100

    added constant file

--- SimpleBot.cs
+++ SimpleBot.cs
@@ -16,8 +16,8 @@ namespace Simple
     {
 
 
-        private string ipAddress = "127.0.0.1";
-        private int port = 8052;
+        private const string ipAddress = constant.IP;
+        private const int port = constant.Port;
         private string tankName;
         private GameObjectState ourMostRecentState;
         private Random random;
