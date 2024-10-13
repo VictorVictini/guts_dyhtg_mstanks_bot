8060475845f9698c58fed353bac008def8bd5529
VictorVictini <84572057+VictorVictini@users.noreply.github.com>
2024-10-13 10:27:55 +0100

    team name

--- APIWrapper.cs
+++ APIWrapper.cs
@@ -2,7 +2,7 @@ namespace Simple
 {
     public static class Constant
     {
-        public const string TeamName = "Uncol_is_Col";
+        public const string TeamName = "Uncol is Col";
         public const string IP = "127.0.0.1";
         public const int Port = 8052;
         public const string ServerIP = "127.0.0.1";
